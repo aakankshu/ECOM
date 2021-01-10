2086901f2213d0eb75438f06f11bd1fe656f1068
aakankshu <aakankshurawat@gmail.com>
2021-01-11 01:03:18 +0530

    Add project files.

---
+++ ECOM/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ECOM.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 283px">
+    <form id="form1" runat="server">
+        <p><h1>
+            THIS IS AN ECOMMERCE WEB APP</h1>&nbsp;</p>
+        <asp:Label ID="Label1" runat="server" Text="CUSTOER NAME:"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server" Height="18px" style="margin-left: 12px; margin-top: 0px"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="margin-left: 31px" Text="Submit" />
+        
+        
+    </form>
+</body>
+</html>
---
+++ ECOM/WebForm1.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace ECOM
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == true)
+            {
+                Label1.Text = ("Your purchase history has been revised!");
+            }
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection ecom_conn = new SqlConnection("Server=tcp:ecomdb29.database.windows.net,1433;Initial Catalog=ecomdb;Persist Security Info=False;User ID=aakankshu;Password=ecomdb@29;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            {
+                SqlCommand insert = new SqlCommand("EXEC dbo.InsertFullName @FullName", ecom_conn);
+                insert.Parameters.AddWithValue("@FullName", TextBox1.Text);
+
+                ecom_conn.Open();
+                insert.ExecuteNonQuery();
+                ecom_conn.Close();
+
+                if (IsPostBack)
+                {
+                    TextBox1.Text = "";
+                }
+            }
+        }
+    }
+}
